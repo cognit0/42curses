b2226a822e7199dcf0b00738cc4a320bed34f560
cogno <c0gnito@192.168.1.100>
2022-03-27 09:27:58 +0300

    Starting FdF

---
+++ FdF/fdf.h
@@ -0,0 +1,15 @@
+#ifndef FDF_H
+# define FDF_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+#include <OpenGL/opengl.h>
+#include "mlx.h"
+#include "mlx_int.h"
+
+
+
+#endif
